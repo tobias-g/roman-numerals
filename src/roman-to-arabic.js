329ac929a5ef1a67f49a1344c2b87ab27f8c4207
Tobias Gray <tobias.gray@outlook.com>
2016-12-03 13:12:08 +0000

    Added logic to convert roman numerals but not subtractive notation yet

--- src/roman-to-arabic.js
+++ src/roman-to-arabic.js
@@ -1,3 +1,15 @@
+'use strict';
+
+const ROMAN_VALUES = {
+    i: 1,
+    v: 5,
+    x: 10,
+    l: 50,
+    c: 100,
+    d: 500,
+    m: 1000
+};
+
 /**
  * Takes a string that is a Roman numeral and returns an int of the Arabic
  * number.
@@ -5,7 +17,14 @@
  * @return {int}                 Arabic numeral
  */
 function romanToArabic(romanNumeral) {
-    return 0; // stub for now
+    let sum = 0;
+
+    // loop through the roman number and add the letter values to a final sum
+    for (let letter of romanNumeral) {
+        sum += ROMAN_VALUES[letter.toLowerCase()] ? ROMAN_VALUES[letter.toLowerCase()] : 0 ;
+    }
+
+    return sum; // stub for now
 }
 
 module.exports = romanToArabic;
